b865d167fb5a5e2112cf4b839c4af33ded6b195a
clara252 <clara.fourrier@enpc.eleves.fr>
2022-11-28 16:28:21 +0100

    Page Index html

--- open/views.py
+++ open/views.py
@@ -1,1 +1,13 @@
 from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<html>" 
+                        "<body>"
+                        "<p><font size='15'color ='blue' >Question"
+                        "</font></p>"
+                        "<form method='post' action='mailto:email@example.com' > "
+                        "Answer :<input type='text' name='Answer' size = 15 maxlenght='18'/><br/> "
+                        "<input type='submit' value='Submit'/>"
+                        "</form>"
+                        "</body>"
+                        "</html>")
--- quizz/urls.py
+++ quizz/urls.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
