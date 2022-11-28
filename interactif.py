92a71142fe299d780bad6539a8d5adf7cb7b67ee
NahelDjf <NahelDjeffal.pro@gmail.com>
2022-11-28 15:02:48 +0100

    Server fonctionnel

---
+++ interactif.py
@@ -0,0 +1,4 @@
+from models import Question
+
+myquestion=create_question()
+myquestion.take_answer()
--- polls/models.py
+++ polls/models.py
@@ -216,5 +216,5 @@ def create_question():
         new_question=Comparison()
     return new_question
 
-myquestion=create_question()
-myquestion.take_answ
+#myquestion=create_question()
+#myquestion.take_answer()
