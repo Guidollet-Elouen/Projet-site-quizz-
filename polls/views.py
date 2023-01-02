76d768fb4cf9c11654f6df69b26f7d1e3241e876
NahelDjf <103316234+NahelDjf@users.noreply.github.com>
2023-01-02 15:13:10 +0100

    test_serveur

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
+from pyexpat.errors import messages
+
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Question
 
@@ -18,3 +20,24 @@ def index(request):
                          "</body>"
                         "</html>")
 
+def take_quiz(request):
+    questions = Question.text_question
+    context = {'questions': questions}
+
+    if request.method == 'GET':
+        request.session['previous_page'] = request.path_info + "?page=" + request.GET.get("page", '1')
+        return render(request, 'quiz.html', context)
+
+    if request.method == 'POST':
+        correct_user_answers = []
+        user_answer = request.POST['option']
+        correct_answer = request.POST.get('answerLabel') #Changer pour database
+        print('correct answer ', correct_answer)
+        print('user answer: ', user_answer)
+        if user_answer == correct_answer:
+            correct_user_answers.append(user_answer)
+            messages.success(request, 'Correct answer')
+            return HttpResponseRedirect(request.session['previous_page'])
+        else:
+            messages.warning(request, f'Wrong answer, Correct Answer is {correct_answer}')
+            return HttpResponseRedirect(request.session['previous_page'])
